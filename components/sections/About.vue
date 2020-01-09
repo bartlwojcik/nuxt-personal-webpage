6692d3c9428d831d5c8d1310c26d001061a34dd8
bartlwojcik <bartlomiejwojcik92@gmail.com>
2020-01-09 21:06:01 +0100

    finish About section

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="section__content">
     <h2 class="mb-6 text-4xl font-display">Who am I?</h2>
-    <p class="pr-12 font-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sit
-      reiciendis nobis, nam nemo cum perferendis in impedit maiores vero
-      pariatur nihil excepturi temporibus enim aliquid veritatis commodi
-      nesciunt. Repudiandae possimus ut quibusdam minus modi reprehenderit,
-      quaerat totam unde dolores? Perferendis maxime aut, iure architecto harum
-      laboriosam molestiae quasi odit!
+    <p class="pr-12 text-sm width-full font-body">
+      <!-- I am a Full Stack Developer and a good design enthusiast.<br /><br />
+      In 2016, after I obtained my Master's degree in Mechatronic Design,<br />
+      I decided to give up the 3D Printing industry and hardware engineering<br />
+      and learn how to code. Now, at the beginning of 2020, I consider it<br />
+      one of the best possible decisions.<br /><br />
+      Since then I've been gaining experience as Frontend Developer,<br />
+      Full Stack Developer, and UX/UI Designer while working both<br />
+      on individual projects and in international teams of several people.<br /><br />
+      I am still developing my coding skills in JavaScript<br />
+      (mainly Vue.js, but I've also worked with React) and PHP (Laravel),<br />
+      but also soft skills required to be a team player, especially during<br />
+      remote work.-->
+      I am a Full Stack Developer and a good design enthusiast.<br /><br />
+      In 2016, after I obtained my Master's degree in Mechatronic Design, I
+      decided to give up the 3D Printing industry and hardware engineering and
+      learn how to code. Now, at the beginning of 2020, I consider it one of the
+      best possible decisions.<br /><br />Since then I've been gaining
+      experience as Frontend Developer, Full Stack Developer, and UX/UI Designer
+      while working bothon individual projects and in international teams of
+      several people.<br /><br />I am still developing my coding skills in
+      JavaScript (mainly Vue.js, but I've also worked with React) and PHP
+      (Laravel), but also soft skills required to be a team player, especially
+      during remote work.
     </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.section_content p {
+  text-align: justify;
+}
+</style>
